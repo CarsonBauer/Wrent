a9e0251a44aad188d4ff7960e5a5703abfbc0f7c
TylerPopson <tjpopson@iu.edu>
2021-02-25 01:38:04 -0500

    new components and pages
    added material-ui
    created dashboard page and made it homepage

---
+++ templates/pages/item-card.js
@@ -0,0 +1,16 @@
+class itemCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            item: '',
+            description: ''
+        }
+    }
+
+    render(){
+        return(
+            <h2>hello</h2>
+            <button>view item</button>
+        );
+    }
+}
