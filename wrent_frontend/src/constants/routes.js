466f7ea035e2700ddb3f2e5260d83670ae778ce3
Yuanming Liu <yli5@iu.edu>
2021-03-03 06:16:16 -0500

    add sign up section.

--- wrent_frontend/src/constants/routes.js
+++ wrent_frontend/src/constants/routes.js
@@ -1,6 +1,7 @@
 import Home from "../components/home";
 // import TherapistPage from "../components/therapists/TherapistPage";
 import SignIn from "../components/auth/SignIn";
+import SignUp from "../components/auth/SignUp";
 export const routes = [
 
 
@@ -9,6 +10,12 @@ export const routes = [
     private: false,
     name: "Login",
     component: SignIn,
+  },
+  {
+    path: "/sign up",
+    private: false,
+    name: "Sign Up",
+    component: SignUp,
   },
   {
     path: "/",
