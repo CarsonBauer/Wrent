8a354e27ec0327f8c8ebe8c744d121a2c29f116f
Yuanming Liu <yli5@iu.edu>
2021-03-03 15:19:24 -0500

    logo

--- wrent_frontend/src/components/auth/SignIn.js
+++ wrent_frontend/src/components/auth/SignIn.js
@@ -11,6 +11,7 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import WrentLogo from './wrentLogo';
 
 
 
@@ -42,6 +43,8 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+
+        <WrentLogo />
 
         <Typography component="h1" variant="h5">
           Sign in
