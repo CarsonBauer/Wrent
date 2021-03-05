b9807ab9acd0c8009dc24191a31d09ada5e08b3e
adasmart <adasmart@iu.edu>
2021-03-05 16:22:52 -0500

    add built code

--- wrent_frontend/src/components/auth/ForgotPassWord.js
+++ wrent_frontend/src/components/auth/ForgotPassWord.js
@@ -7,10 +7,11 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import WrentLogo from './wrentLogo';
 
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        padding: theme.spacing(5),
+        marginLeft: -theme.spacing(3),
+        width: '450px',
     },
     avatar: {
         margin: theme.spacing(1),
@@ -40,44 +44,44 @@ export default function ForgotPassWord() {
 
     return (
         <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <WrentLogo />
-                <Typography component="h1" variant="h5">
-                    Password Assistance
-        </Typography>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
+            <Paper className={classes.paper}>
+                <CssBaseline/>
+                    <WrentLogo/>
+                    <Typography component="h1" variant="h5">
+                        Password Assistance
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
                         </Grid>
-                    </Grid>
 
 
-                    <Grid item>
-                        <Button
-                            href="/afterreturncode"
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Get Verification Code
-          </Button>
-                    </Grid>
+                        <Grid item>
+                            <Button
+                                href="/afterreturncode"
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Get Verification Code
+                            </Button>
+                        </Grid>
 
 
-                </form>
-            </div>
-        </Container >
+                    </form>
+            </Paper>
+        </Container>
     );
 }
