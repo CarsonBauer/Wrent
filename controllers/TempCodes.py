e2e719ab4f59b05dad364fffb1134023c6344d7a
Carson Bauer <bauer.carson@gmail.com>
2021-03-06 13:57:53 -0500

    Backend changes integrated. (Login now works on front end with changes)

--- controllers/TempCodes.py
+++ controllers/TempCodes.py
@@ -25,31 +25,32 @@ def send_code():
 
         TempCodes.post_code(random_string)
 
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login("bauer.carson@gmail.com", os.getenv('CONNP'))
-        server.sendmail("bauer.carson@gmail.com", email, random_string)
+        # server = smtplib.SMTP("smtp.gmail.com", 587)
+        # server.starttls()
+        # server.login("email", os.getenv('CONNP'))
+        # server.sendmail("email", email, random_string)
 
-        return random_string
+        return jsonify(random_string),200
     else:
         return jsonify(isError=True,
                        message="Email not found",
                        statusCode=404,
                        data=str("Not found")), 404
 
-@controllers.route('/tempcodes', methods=['GET'])
+@controllers.route('/tempcodes/check', methods=['POST'])
 def check_code():
     auth = request.json
     code = TempCodes.query.filter_by(code=auth['code']).first()
 
     if code:
         user = Users.query.filter_by(email=auth['email']).first()
-        token = create_access_token({
-            "userName": user.userName,
-            "email": user.email,
-            "permission": user.permission
-            })
-        return jsonify({"access_token": token}), 200
+        # token = create_access_token({
+        #     "userName": user.userName,
+        #     "email": user.email,
+        #     "permission": user.permission
+        #     })
+        # return jsonify({"access_token": token}), 200
+        return jsonify({'password': user.password}), 200
     else:
         return jsonify(isError=True,
                        message="Invalid retrieval code.",
