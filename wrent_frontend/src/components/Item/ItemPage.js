47f83943dad877f47a84fdfd8ee55462d57f83d4
TylerPopson <tjpopson@iu.edu>
2021-03-11 10:58:56 -0500

    added owner info to item page

--- wrent_frontend/src/components/Item/ItemPage.js
+++ wrent_frontend/src/components/Item/ItemPage.js
@@ -11,7 +11,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Paper from "@material-ui/core/Paper";
-import WrentLogo from '../auth/wrentLogo';
+import Avatar from "@material-ui/core/Avatar";
+import Image from './img/background.jpg';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -26,16 +27,36 @@ const useStyles = makeStyles((theme) => ({
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing(3),
     },
+    description: {
+        width: '50%',
+        marginTop: theme.spacing(3),
+    },
+    imageContainer: {
+        width: '50%',
+        maxHeight: '300px',
+        marginTop: theme.spacing(3),
+    },
+    image: {
+        maxHeight: '300px'
+    },
+    ownerData: {
+        maxHeight: '300px',
+        width: '50%'
+    },
+    rentButton: {
+        width: '12%',
+        height: '50px'
+    },
 }));
 
 export default function ItemPage() {
     const classes = useStyles();
 
     var id = "";
-    var image = "";
-    var description = "";
+    //var image = {Image};
+    var description = "Donec interdum dolor sed finibus posuere. Curabitur dignissim tellus et ultrices rutrum. Curabitur vestibulum, nisi ac faucibus posuere, leo ipsum ultricies enim, ut consequat quam erat et velit. Nulla id mauris neque. Mauris dui velit, scelerisque in elit et, fringilla tristique augue. Sed feugiat tellus velit, sed maximus ipsum posuere vel. Pellentesque cursus finibus lacus in blandit. Nam auctor risus quis diam vestibulum, sed ornare mi pharetra. Aenean lectus est, malesuada luctus leo eu, eleifend pretium diam. Donec lacinia, arcu vitae tempor accumsan, quam turpis tristique est, quis pulvinar metus nunc at purus. Vivamus efficitur sapien eu laoreet scelerisque. Praesent lobortis est nulla, non dignissim tellus luctus quis. Praesent sed lectus ut neque semper porttitor. Curabitur eu ex sapien. Etiam vitae augue tortor. Sed magna tortor, pretium vestibulum orci ut, tincidunt imperdiet dolor.";
     var ownerId = "";
-    var name = "";
+    var name = "Item Name";
     var location = "";
     var rating = "";
 
@@ -44,8 +65,42 @@ export default function ItemPage() {
             <CssBaseline />
             <Paper className={classes.paper}>
                 
-                <Grid container direction="row" justify="center" alignItems="center">
-                    //image
+                <Grid container direction="row" justify='flex-start' alignItems="center">
+                    <Typography variant='h3'>
+                        {name}
+                    </Typography>
+                </Grid>
+
+                <Grid container direction="row" justify='flex-start' alignItems="center">
+                    <Button className={classes.rentButton} variant='contained' color='Primary'>Rent Item</Button>
+                    <Button className={classes.rentButton} variant='outlined' color='secondary'>Add To Cart</Button>
+                </Grid>
+
+                <Grid container direction="row" justify='flex-start' alignItems="flex-start">
+                    <Grid item className={classes.description}>
+                        
+                        <Typography variant='body1'>{description}</Typography>
+                    </Grid>
+                    <Grid item className={classes.imageContainer}>
+                        <img className = {classes.image} src={Image} class="img-Rounded"></img>
+                    </Grid>
+                </Grid>
+
+                <Grid container direction='row' justify='flex-start' alignItems="center">
+                    <Grid className={classes.ownerData} item justify='center' alignItems='center'>
+                        <Grid container direction='row' justify='flex-start' alignItems="flex-start">
+                            <Typography variant='h5'>Owner Info</Typography>
+                        </Grid>
+                        <Grid container direction='row' justify='flex-start' alignItems="flex-start">
+                            <Grid item>
+                                <Avatar>JD</Avatar>
+                                <Typography variant='h6'>John Doe <Button variant='text' color='secondary'>Contact Owner</Button></Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        TODO Map 
+                    </Grid>
                 </Grid>
                 
             </Paper>
--- wrent_frontend/src/components/home/Item.js
+++ wrent_frontend/src/components/home/Item.js
@@ -3,7 +3,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import card from "@material-ui/core/card";
+import Card from "@material-ui/core/card";
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
