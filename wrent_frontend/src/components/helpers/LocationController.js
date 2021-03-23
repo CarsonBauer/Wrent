8ddf979bb05a6f00d02b916aa3eeb2a924d7a8fb
Carson Bauer <bauer.carson@gmail.com>
2021-03-22 21:31:07 -0400

    Helper changes

--- wrent_frontend/src/components/helpers/LocationController.js
+++ wrent_frontend/src/components/helpers/LocationController.js
@@ -1,17 +1,55 @@
-module.exports = {
-    fetchLocation: async (loc) => {
-        const res = await fetch('/locations/'+loc, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        const data = await res.json();
-        return data
-    },
+// module.exports = {
+//     fetchLocation: async (loc) => {
+//         const res = await fetch('/locations/'+loc, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-type': 'application/json'
+//             }
+//         })
+//         const data = await res.json();
+//         return data
+//     },
 
-    postLocation: async (location) => {
-        const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.REACT_APP_API_KEY}`)
+//     postLocation: async (location) => {
+//         const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.REACT_APP_API_KEY}`)
+//         const res_json = await res.json()
+//         if (res_json['status'] == "ZERO_RESULTS") {
+//             return "ZERO_RESULTS"
+//         } else {
+//             var lt = res_json.results[0].geometry.location.lat
+//             var lg = res_json.results[0].geometry.location.lng
+//             const data = await fetch('/locations', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-type': 'application/json',
+//                     'Authorization': `Bearer ${localStorage.getItem('user-jwt')}`
+//                 },
+//                 body: JSON.stringify(
+//                     {
+//                         lat: lt,
+//                         lon: lg
+//                     }
+//                 )
+//             })
+//             var data_json = await data.json();
+//             return data_json['data']
+//         }
+//     }
+// }
+
+export async function fetchLocation(loc) {
+    const res = await fetch('/locations/'+loc, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    const data = await res.json();
+    return data
+}
+
+export async function postLocation(location) {
+    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.REACT_APP_API_KEY}`)
         const res_json = await res.json()
         if (res_json['status'] == "ZERO_RESULTS") {
             return "ZERO_RESULTS"
@@ -34,5 +72,4 @@ module.exports = {
             var data_json = await data.json();
             return data_json['data']
         }
-    }
 }
--- wrent_frontend/src/components/helpers/RentalController.js
+++ wrent_frontend/src/components/helpers/RentalController.js
@@ -1,13 +1,25 @@
-module.exports = {
-    getRentalItems: async (id) => {
-        const res = await fetch(`/rentals/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('user-jwt')}`
-            }
-        })
-        const data = await res.json();
-        return data
-    }
+// module.exports = {
+//     getRentalItems: async (id) => {
+//         const res = await fetch(`/rentals/${id}`, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-type': 'application/json',
+//                 'Authorization': `Bearer ${localStorage.getItem('user-jwt')}`
+//             }
+//         })
+//         const data = await res.json();
+//         return data
+//     }
+// }
+
+export async function getRentalItems(id) {
+    const res = await fetch(`/rentals/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('user-jwt')}`
+        }
+    })
+    const data = await res.json();
+    return data
 }
