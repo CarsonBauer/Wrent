d7c1bef7d4781ad30649abea24d1ba23cf380d70
TylerPopson <tjpopson@iu.edu>
2021-04-08 09:43:56 -0400

    adjusted graph properties

--- wrent_frontend/src/components/AdminPage/Deposits.js
+++ wrent_frontend/src/components/AdminPage/Deposits.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { PieChart, Pie, Label, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 import Title from './Title';
+//import { Tooltip } from '@material-ui/core';
 
 function preventDefault(event) {
     event.preventDefault();
@@ -17,41 +18,26 @@ const useStyles = makeStyles({
 
 const data01 = [
     {
-      "name": "Group A",
-      "value": 400
+      "name": "Rented",
+      "value": 500
     },
     {
-      "name": "Group B",
+      "name": "not rented",
       "value": 300
-    },
-    {
-      "name": "Group C",
-      "value": 300
-    },
-    {
-      "name": "Group D",
-      "value": 200
-    },
-    {
-      "name": "Group E",
-      "value": 278
-    },
-    {
-      "name": "Group F",
-      "value": 189
     }
   ];
 
 export default function Deposits() {
     const classes = useStyles();
     return (
-
+      <>
+        <Title>Items</Title>
         <ResponsiveContainer>
             <PieChart width={730} height={250}>
-                <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label/>
-                
+                <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={55} fill="#679ECE" label/>
+                <Tooltip/>
             </PieChart>
         </ResponsiveContainer>
-
+      </>
     );
 }
