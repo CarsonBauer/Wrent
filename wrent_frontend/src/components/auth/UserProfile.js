84f818c343ad0695118b6c98e3dd214515c9a395
Yuanming Liu <yli5@iu.edu>
2021-04-08 16:34:09 -0400

    delete user portrait in user profile

--- wrent_frontend/src/components/auth/UserProfile.js
+++ wrent_frontend/src/components/auth/UserProfile.js
@@ -33,11 +33,79 @@ import { fetchLocation } from "../helpers/LocationController"
 
 
 const useStyles = makeStyles((theme) => ({
+    paper: {
+        border: '10px solid #6f6cb2',
+        width: '300%',
+        color: '#4E4BA6',
+        backgroundColor: 'white',
+        borderRadius: '5px',
+        padding: '30px',
+        marginLeft: '-400px',
+    },
+    d1: {
+        paddingLeft: '15px',
+        fontSize: '30px',
+        color: 'black',
+        float: 'right',
+        width: "40%",
+        border: '10px #4E4BA6',
+        borderRadius: '5px',
+        marginRight: '60px',
+    },
+    t1:
+    {
+        width: '70%',
+        borderTop: '10px',
+        borderBottom: '10px',
+        fontSize: '20px',
+        color: 'white',
+        backgroundColor: '#679ece',
+        borderRadius: '5px',
+        padding: '8px',
+        paddingLeft: '30px',
+        textAlign: 'center',
+    },
+    t2: {
+        width: '30%',
+        borderTop: '10px',
+        borderBottom: '10px',
+        fontSize: '20px',
+        color: 'white',
+        backgroundColor: '#679ece',
+        borderRadius: '5px',
+        padding: '8px',
+        paddingLeft: '80px',
+    },
+    t3:
+    {
+        width: '30%',
+        borderTop: '10px',
+        borderBottom: '10px',
+        fontSize: '20px',
+        color: 'white',
+        backgroundColor: '#679ece',
+        borderRadius: '5px',
+        padding: '8px',
+        paddingLeft: '80px',
 
-
+    },
+    t4:
+    {
+        width: '30%',
+        borderTop: '10px',
+        borderBottom: '10px',
+        fontSize: '20px',
+        color: 'white',
+        backgroundColor: '#679ece',
+        borderRadius: '5px',
+        padding: '8px',
+        paddingLeft: '80px',
+    }
 }));
 
 export default function UserProfile(props) {
+
+    const classes = useStyles();
 
     function changeBackground(e) {
         e.target.style.background = '#997cb3';
@@ -85,31 +153,17 @@ export default function UserProfile(props) {
         return data
     }
 
-
-
-
-
-
-
-
-
-
-
     return (
         <Container component="main" maxWidth="xs">
             <br />
-            <Paper style={{ border: '10px solid #6f6cb2', width: '300%', color: '#4E4BA6', backgroundColor: 'white', borderRadius: '5px', padding: '40px', marginLeft: '-400px', }} className="paper">
-                <img src={userImage} style={{ width: "10%", border: '1px solid black', borderRadius: '100px', marginLeft: '90px' }} alt="UserImage" />
-
-                <img src={plusIcon} style={{ width: "2%", border: '1px solid ', borderRadius: '5px', marginLeft: '-8px' }} alt="plusIcon" />
-                <div style={{ paddingLeft: '15px', fontSize: '30px', color: 'black', float: 'right', width: "40%", border: '10px #4E4BA6', borderRadius: '5px', marginRight: '60px' }}>
-                    <Typography onMouseOut={changeBackground2} onMouseOver={changeBackground} style={{ width: '80%', borderTop: '10px', borderBottom: '10px', fontSize: '25px', color: 'white', backgroundColor: '#679ece', borderRadius: '5px', padding: '5px', textAlign: 'center' }} component="h1" variant="h5">
+            <Paper className={classes.paper}>
+                <div className={classes.d1}>
+                    <Typography className={classes.t1} onMouseOut={changeBackground2} onMouseOver={changeBackground} component="h1" variant="h5">
                         Map
                 </Typography>
                     <br />
                     &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
                     <Button href={`/map/${location['lat']}/${location['lon']}`} >View On Map</Button>
-
                     <br />
                     <br />
                     <br />
@@ -121,7 +175,6 @@ export default function UserProfile(props) {
                     <br />
                     <br />
                 </div>
-
                 <>
                     {items.map((item, i) => (
                         <Grid item xs={2}>
@@ -129,21 +182,19 @@ export default function UserProfile(props) {
                         </Grid>
                     ))}
                 </>
-                <br />
-                <br />
-                <Typography onMouseOut={changeBackground2} onMouseOver={changeBackground} style={{ width: '37%', borderTop: '10px', borderBottom: '10px', fontSize: '25px', color: 'white', backgroundColor: '#679ece', borderRadius: '5px', padding: '5px', textAlign: 'center' }} component="h1" variant="h5">
+                <Typography className={classes.t2} onMouseOut={changeBackground2} onMouseOver={changeBackground} component="h1" variant="h5">
                     User Profile
                 </Typography>
                 <br />
                 <br />
-                <Typography onMouseOut={changeBackground2} onMouseOver={changeBackground} style={{ width: '30%', borderTop: '10px', borderBottom: '10px', fontSize: '20px', color: 'white', backgroundColor: '#679ece', borderRadius: '5px', padding: '8px', paddingLeft: '80px', textAlign: 'center' }} component="h1" variant="h5">
+                <Typography className={classes.t3} onMouseOut={changeBackground2} onMouseOver={changeBackground} component="h1" variant="h5">
                     {/* UserName :  &nbsp; &nbsp;&nbsp;&nbsp; {users.name} */}
                     User Name :  &nbsp; &nbsp;&nbsp;&nbsp; {users.name}
                     {users.lastName} &nbsp; &nbsp;&nbsp;&nbsp; {users.firstName}
                 </Typography>
                 <br />
                 <br />
-                <Typography onMouseOut={changeBackground2} onMouseOver={changeBackground} style={{ width: '30%', borderTop: '10px', borderBottom: '10px', fontSize: '20px', color: 'white', backgroundColor: '#679ece', borderRadius: '5px', padding: '8px', paddingLeft: '80px', textAlign: 'center' }} component="h1" variant="h5">
+                <Typography className={classes.t4} onMouseOut={changeBackground2} onMouseOver={changeBackground} component="h1" variant="h5">
                     Email :  &nbsp; &nbsp;&nbsp;&nbsp; {users.email}
                 </Typography>
                 <br />
